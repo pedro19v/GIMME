d09c07319c340765b0ba1ce58099f395e8ec390e
SamGomes <sam.s.gomes@sapo.pt>
2021-05-01 14:42:22 +0100

    Updated API version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="GIMMECore",
-    version="1.2.0",
+    version="1.3.0",
     license = "CC BY 4.0",
     author="Samuel Gomes",
     author_email = "samuel.gomes@tecnico.ulisboa.pt",
